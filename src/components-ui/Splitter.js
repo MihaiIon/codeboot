da7a8565ffc6463a643e2678ae343d872c09ad01
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-02-01 12:31:59 -0500

    Added empty components for later

---
+++ src/components-ui/Splitter.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Splitter extends Component {
+  render() {
+    return <div className="c-splitter" />;
+  }
+}
+
+Splitter.propTypes = {};
+
+export default Splitter;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Layout extends Component {
+  render() {
+    return <div className="c-layout" />;
+  }
+}
+
+Layout.propTypes = {};
+
+export default Layout;
