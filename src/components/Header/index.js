b1fadaf0bb65b17fb417cf135e35101a28c9d996
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-04-06 18:41:42 -0400

    fixed header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,42 @@
-import { connect } from "react-redux";
+import React, { useState, useContext } from "react";
 
-// Import component
-import Header from "./Header";
+// Helpers
+import classNamesHelper from "classnames-helper";
 
-const mapStateToProps = ({ file }) => ({});
+// Components
+import Logo from "./components/Logo";
+import List from "./components/_base/HeaderList";
+import Navigation from "./components/_base/HeaderNav";
 
-const mapDispatchToProps = () => ({});
+// Super components
+import DownloadFile from "./components/DownloadFile";
+import NewFile from "./components/NewFile";
+import Options from "./components/Options";
+import Controls from "./components/Controls";
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Header);
+function Header() {
+  return (
+    <div className={classNamesHelper("c-header o-wrapper", "o-layout -flex")}>
+      <Navigation>
+        <Logo />
+        <div className="c-header_separator o-layout_item" />
+        <List>
+          <NewFile />
+          {/* <DownloadFile /> */}
+        </List>
+      </Navigation>
+      <Navigation linked>
+        <List>
+          <Controls step />
+          <Controls playWithSteps />
+          <Controls play />
+          <Controls stop />
+        </List>
+      </Navigation>
+    </div>
+  );
+}
+
+Header.propTypes = {};
+
+export default Header;
